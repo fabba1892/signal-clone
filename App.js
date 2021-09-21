029c0470bab84613743e0514082de44d170c8455
fabba1892 <fabian.vanwyk@gmail.com>
2021-09-21 12:42:53 +0200

    login screen and stack created

--- App.js
+++ App.js
@@ -2,14 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import LoginScreen from './screens/LoginScreen';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <Text>Lets build signal</Text>
-        <StatusBar style="auto" />
-      </View>
+      <Stack.Screen name='Login' component={LoginScreen} />
     </NavigationContainer>
   );
 }
