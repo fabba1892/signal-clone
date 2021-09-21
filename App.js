350a6c0c7dd1bc56b1f25612be609ebea74cc8f0
fabba1892 <fabian.vanwyk@gmail.com>
2021-09-21 12:29:05 +0200

    add the nav container

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
-import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Lets build signal</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>Lets build signal</Text>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
 
