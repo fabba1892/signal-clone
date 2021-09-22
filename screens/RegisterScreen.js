71e7fa4be4996a988005c1488e733bf332a21971
fabba1892 <fabian.vanwyk@gmail.com>
2021-09-22 15:21:56 +0200

    keybrd view when view comes up

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { KeyboardAvoidingView } from 'react-native';
+import { Button, Input } from 'react-native-elements';
+import { StatusBar } from "expo-status-bar";
 
 const RegisterScreen = ({ navigation }) => {
+    const [name, setName] = useState('')
+
     return (
-        <View>
-            <Text>Iam register screen</Text>
-        </View>
-    )
-}
+        <KeyboardAvoidingView behavior="padding" style={styles.container}>
+            <StatusBar style="light" />
+
+            <Text h3 style={{ marginBottom: 50 }}>
+                Create a Signal Account
+            </Text>
+
+            <View style={styles.inputContainer}>
+                <Input placeholder="Full Name" autofocus type="text" />
+            </View>
+        </KeyboardAvoidingView>
+    );
+};
 
 export default RegisterScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {},
+    inputContainer: {},
+});
