036db354f5e19851c9201244fe59e9f3b81bf902
fabba1892 <fabian.vanwyk@gmail.com>
2021-09-28 21:50:11 +0200

    firebase db and auth setup

--- firebase.js
+++ firebase.js
@@ -1,3 +1,7 @@
+import * as firebase from "firebase";
+import "firebase/firestore";
+import "firebase/auth";
+
 const firebaseConfig = {
     apiKey: "AIzaSyC8UDzQk7ugwu5HgpPAZZ4fBrqJljISYvU",
     authDomain: "signal-clone-yt-30bc7.firebaseapp.com",
@@ -6,3 +10,16 @@ const firebaseConfig = {
     messagingSenderId: "16696159511",
     appId: "1:16696159511:web:48049e7758772a0bdf1b52"
 };
+
+let app;
+
+if (firebase.apps.length === 0)  {
+    app = firebase.initializeApp(firebaseConfig);
+} else {
+    app = firebase.app();
+}
+
+const db = app.firestore();
+const auth = firebase.auth();
+
+export {db, auth};
