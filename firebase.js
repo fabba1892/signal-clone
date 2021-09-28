7f7000553abae29f7807691dffb4b04585e487d8
fabba1892 <fabian.vanwyk@gmail.com>
2021-09-28 22:38:45 +0200

    firebase import export corrected

--- firebase.js
+++ firebase.js
@@ -1,4 +1,4 @@
-import * as firebase from "firebase";
+import firebase from 'firebase/app';
 import "firebase/firestore";
 import "firebase/auth";
 
@@ -12,14 +12,13 @@ const firebaseConfig = {
 };
 
 let app;
-
-if (firebase.apps.length === 0)  {
-    app = firebase.initializeApp(firebaseConfig);
+if (firebase.apps.length === 0) {
+  app = firebase.initializeApp(firebaseConfig);
 } else {
-    app = firebase.app();
+  app = firebase.app();
 }
 
 const db = app.firestore();
 const auth = firebase.auth();
 
-export {db, auth};
+export { db, auth };
