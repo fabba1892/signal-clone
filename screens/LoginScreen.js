f768f3ada9e379c876016b9513b768cfaa3e7d0f
fabba1892 <fabian.vanwyk@gmail.com>
2021-09-29 21:09:59 +0200

    auth listener

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,12 +1,23 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { Button, Input, Image } from "react-native-elements";
 import { StatusBar } from "expo-status-bar";
 import { KeyboardAvoidingView } from 'react-native';
+import { auth } from '../firebase';
 
 const LoginScreen = ({ navigation }) => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
+
+useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+        if(authUser) {
+            navigation.navigate.replace("Home");
+        }
+    })
+    return unsubscribe;
+}, [])
+
     const signIn = () => { };
 
     return (
