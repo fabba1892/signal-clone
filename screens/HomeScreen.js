217e4836fd0b1e182420338cc2c01ea689608979
fabba1892 <fabian.vanwyk@gmail.com>
2021-09-30 22:05:45 +0200

    signIn and signOut

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,39 @@
 import React, { useLayoutEffect } from "react";
-import { StyleSheet, Text, View, ScrollView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
+import { Avatar } from "react-native-elements";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { AntDesign, SimpleLineIcons } from "@expo/vector-icons";
 import CustomListItem from "../components/CustomListItem";
+import { auth, db } from "../firebase";
 
 const HomeScreen = ({ navigation }) => {
+  const signOutUser = () => {
+    auth.signOut().then(() => {
+      navigation.replace("Login");
+    });
+  };
+
   useLayoutEffect(() => {
     navigation.setOptions({
       title: "Welcome to Signal",
+      headerStyle: { backgroundColor: "#fff" },
+      headerTitleStyle: { color: "black" },
+      headerTintColor: "black",
+      headerLeft: () => (
+        <View style={{ marginLeft: 20 }}>
+          <TouchableOpacity>
+            <Avatar rounded source={{ uri: auth?.currentUser?.photoURL }} />
+          </TouchableOpacity>
+        </View>
+      ),
     });
-  }, [navigation]);
+  }, []);
 
   return (
     <SafeAreaView>
