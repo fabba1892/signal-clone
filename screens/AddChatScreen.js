3cfa5c0a1046f5181d938b7d283e7ba5e899b9ea
fabba1892 <fabian.vanwyk@gmail.com>
2021-10-07 21:52:17 +0200

    chatscreen icon and input

---
+++ screens/AddChatScreen.js
@@ -0,0 +1,33 @@
+import React, { useLayoutEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Button, Icon, Input } from "react-native-elements";
+
+const AddChatScreen = ({ navigation }) => {
+  const [input, setInput] = useState("");
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: "Add a new Chat",
+      headerBackTitle: "Chats",
+    });
+  }, [navigation]);
+
+  return (
+    <View style={styles.container}>
+      <Input
+        placeholder="Enter a chat name"
+        value={input}
+        onChangeText={(text) => setInput(text)}
+        leftIcon={
+          <Icon name="wechat" type="antdesign" size={24} color="black" />
+        }
+      />
+    </View>
+  );
+};
+
+export default AddChatScreen;
+
+const styles = StyleSheet.create({
+  container: {},
+});
