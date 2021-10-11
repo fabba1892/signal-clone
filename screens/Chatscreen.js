4d2184f3bb0f34d4f4115169cd43864520060146
fabba1892 <fabian.vanwyk@gmail.com>
2021-10-11 20:45:43 +0200

    chatscreen params etc

--- screens/Chatscreen.js
+++ screens/Chatscreen.js
@@ -1,10 +1,30 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { Avatar } from "react-native-elements";
 
-const Chatscreen = () => {
+const Chatscreen = ({ navigation, route }) => {
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: "Chat",
+      headerBackTitleVisible: false,
+      headerTitleAlign: "left",
+      headerTitle: () => (
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <Avatar rounded source={{ uri: "" }} />
+          <Text>{route.params.chatName}</Text>
+        </View>
+      ),
+    });
+  }, [navigation]);
+
   return (
     <View>
-      <Text>I am the chat</Text>
+      <Text>{route.params.chatName}</Text>
     </View>
   );
 };
